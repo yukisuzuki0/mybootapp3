bf27308dce4b16aafe81b923cbe188362da36459
yukisuzuki0 <te220478@gmail.com>
2025-05-28 10:45:31 +0900

    comit

---
+++ src/main/java/com/example/mybootapp3/Mybootapp3Application.java
@@ -0,0 +1,13 @@
+package com.example.mybootapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybootapp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybootapp3Application.class, args);
+	}
+
+}
