e7fb01db7fb7a39da4386c4a38cabd00099dba6f
yukisuzuki0 <te220478@gmail.com>
2025-06-06 14:14:34 +0900

    Revert "comit"

---
+++ src/test/java/com/example/mybootapp3/Mybootapp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybootapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybootapp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
